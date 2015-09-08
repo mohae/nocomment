6c2acbc17af21f21bf8f428b21cce6cd2b557542
Joel Scoble <joel.scoble@outlook.com>
2015-09-08 18:50:29 +0000

    lex comments and new lines

---
+++ lex.go
@@ -0,0 +1,306 @@
+// Copyright 2015 Joel Scoble.
+// This code is governed by the MIT license, please
+// refer to the LICENSE file.
+//
+// the design of these tests is based on the tests in
+// http://golang.org/src/text/template/parse/lex_test.go
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package decomment
+
+import (
+	"fmt"
+	"strings"
+	//"unicode"
+	"unicode/utf8"
+)
+
+// Pos is a byte position in the original input text.
+type Pos int
+
+type token struct {
+	typ   tokenType
+	pos   Pos
+	value string
+}
+
+func (t token) String() string {
+	switch {
+	case t.typ == tokenEOF:
+		return "EOF"
+	case t.typ == tokenError:
+		return t.value
+	}
+	return fmt.Sprintf("%q", t.value)
+}
+
+const lineCommentSlash = "//"
+const lineCommentHash = "#"
+const blockCommentBegin = "/*"
+const blockCommentEnd = "*/"
+const cr = '\r'
+const nl = '\n'
+
+type tokenType int
+
+const (
+	tokenError tokenType = iota
+	tokenEOF
+	tokenText              // anything that isn't one of the following
+	tokenCommentSlash      // //
+	tokenCommentHash       // #
+	tokenBlockCommentStart // /*
+	tokenBlockCommentEnd   // */
+	tokenNL                // \n
+	tokenCR                // \r
+)
+
+var key = map[string]tokenType{
+	"//": tokenCommentSlash,
+	"#":  tokenCommentHash,
+	"/*": tokenBlockCommentStart,
+	"*/": tokenBlockCommentEnd,
+	"\n": tokenNL,
+	"\r": tokenCR,
+}
+
+type commentType int
+
+const (
+	none commentType = iota
+	commentSlash
+	commentHash
+	commentBlock
+)
+
+var CommentStrings = [...]string{
+	none:         "none",
+	commentSlash: "line comment: //",
+	commentHash:  "line comment: #",
+	commentBlock: "block comment",
+}
+
+const eof = -1
+
+type stateFn func(*lexer) stateFn
+
+type lexer struct {
+	name       string     // the name of the imput; used for errors
+	input      string     // the string being scanned
+	rightDelim string     // start of whatever is being delimited
+	leftDelim  string     // end of whatever is being delimited
+	state      stateFn    // the next lexing function to enter
+	pos        Pos        // current position of this item
+	start      Pos        // start position of this item
+	width      Pos        // width of last rune read from input
+	lastPos    Pos        // position of most recent item returned by nextItem
+	tokens     chan token // channel of scanned tokens
+	parenDepth int        // nesting depth of () exprs <- probably not needed
+	commentTyp commentType
+}
+
+// accept consumes the next rune if it's from the valid set.
+func (l *lexer) accept(valid string) bool {
+	if strings.IndexRune(valid, l.next()) >= 0 {
+		return true
+	}
+	l.backup()
+	return false
+}
+
+// acceptRun consumes a run of runes from the valid set
+func (l *lexer) acceptRun(valid string) {
+	for strings.IndexRune(valid, l.next()) >= 0 {
+	}
+	l.backup()
+}
+
+// backup steps back one rune. Can be called only once per call of next.
+func (l *lexer) backup() {
+	l.pos -= l.width
+}
+
+// emit passes an item back to the client.
+func (l *lexer) emit(t tokenType) {
+	l.tokens <- token{t, l.start, l.input[l.start:l.pos]}
+	l.start = l.pos
+}
+
+// error returns an error token and terminates the scan by passing back a nil
+// pointer that will be the next state, terminating l.run.
+func (l *lexer) errorf(format string, args ...interface{}) stateFn {
+	l.tokens <- token{tokenError, l.start, fmt.Sprintf(format, args...)}
+	return nil
+}
+
+// ignore skips over the pending input before this point.
+func (l *lexer) ignore() {
+	l.start = l.pos
+}
+
+// next returns the next rune in the input.
+func (l *lexer) next() rune {
+	if int(l.pos) >= len(l.input) {
+		l.width = 0
+		return eof
+	}
+	r, w := utf8.DecodeRuneInString(l.input[l.pos:])
+	l.width = Pos(w)
+	l.pos += l.width
+	return r
+}
+
+// nextItem returns the next item from the input.
+func (l *lexer) nextToken() token {
+	for {
+		select {
+		case token := <-l.tokens:
+			return token
+		default:
+			l.state = l.state(l)
+		}
+	}
+	panic("not reached")
+}
+
+// peek returns but does not consume the next rune in the input
+func (l *lexer) peek() rune {
+	r := l.next()
+	l.backup()
+	return r
+}
+
+// run lexes the input by executing state functions until the state is nil.
+func (l *lexer) run() {
+	for state := lexText; state != nil; {
+		state = state(l)
+	}
+	close(l.tokens) // No more tokens will be delivered
+}
+
+func lex(name, input string) *lexer {
+	l := &lexer{
+		name:   name,
+		input:  input,
+		state:  lexText,
+		tokens: make(chan token, 2),
+	}
+	go l.run() // concurrently run state machine
+	return l
+}
+
+// lexLineComment handles scanning of line comments.
+// Line comments start with either # or // and end with a new line.
+func lexLineComment(l *lexer) stateFn {
+	// based on type consume the start
+	switch l.commentTyp {
+	case commentSlash:
+		l.pos += Pos(len(lineCommentSlash))
+	case commentHash:
+		l.pos += Pos(len(lineCommentHash))
+	}
+	// scan until the comment is consumed: EOL is encountered
+	for {
+		t := l.next()
+		if t == cr {
+			// if this is \r\n consume the \n
+			if l.peek() == nl {
+				t = l.next()
+			}
+			break
+		}
+		if t == nl {
+			break
+		}
+		if t == eof {
+			break
+		}
+	}
+	// comment is done, ignore processed runes and continue lexing
+	l.ignore()
+	return lexText
+}
+
+// lexBlockComment handles the scanning of block comments.
+// Block comments start with a /* and end with */. They may
+// span new lines
+func lexBlockComment(l *lexer) stateFn {
+	l.pos += Pos(len(blockCommentBegin))
+	// find end of comment or error if none
+	i := strings.Index(l.input[l.pos:], blockCommentEnd)
+	if i < 0 {
+		return l.errorf("unclosed block comment")
+	}
+	l.pos += Pos(i + len(blockCommentEnd))
+	if !strings.HasPrefix(l.input[l.pos:], l.rightDelim) {
+		return l.errorf("comment ends before */")
+	}
+	//l.pos += Pos(len(blockCommentEnd))
+	l.ignore()
+	return lexText
+	// comment is done, ignore processed runes and continue lexing
+}
+
+// lexReturn handles a carriage return, `\r`.
+func lexReturn(l *lexer) stateFn {
+	l.pos += Pos(len(string(cr)))
+	l.emit(tokenCR)
+	return lexText
+}
+
+// lexNewLine handles a new line, `\n`
+func lexNewLine(l *lexer) stateFn {
+	l.pos += Pos(len(string(nl)))
+	l.emit(tokenNL)
+	return lexText
+}
+
+// stateFn to process input and tokenize things
+func lexText(l *lexer) stateFn {
+	for {
+		if strings.HasPrefix(l.input[l.pos:], lineCommentSlash) {
+			if l.pos > l.start {
+				l.emit(tokenText)
+			}
+			l.commentTyp = commentSlash
+			return lexLineComment // next state
+		}
+		if strings.HasPrefix(l.input[l.pos:], lineCommentHash) {
+			if l.pos > l.start {
+				l.emit(tokenText)
+			}
+			l.commentTyp = commentHash
+			return lexLineComment
+		}
+		if strings.HasPrefix(l.input[l.pos:], blockCommentBegin) {
+			if l.pos > l.start {
+				l.emit(tokenText)
+			}
+			l.commentTyp = commentBlock
+			return lexBlockComment
+		}
+		t := l.peek()
+		if t == cr {
+			if l.pos > l.start {
+				l.emit(tokenText)
+			}
+			return lexReturn
+		}
+		if t == nl {
+			if l.pos > l.start {
+				l.emit(tokenText)
+			}
+			return lexNewLine
+		}
+		if l.next() == eof {
+			break
+		}
+	}
+	// Correctly reached EOF.
+	if l.pos > l.start {
+		l.emit(tokenText)
+	}
+	l.emit(tokenEOF) // Useful to make EOF a token
+	return nil       // Stop the run loop.
+}
---
+++ lex_test.go
@@ -0,0 +1,117 @@
+// Copyright 2015 Joel Scoble.
+// This code is governed by the MIT license, please
+// refer to the LICENSE file.
+//
+// the design of these tests is based on the tests in
+// http://golang.org/src/text/template/parse/lex_test.go
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package decomment
+
+import (
+	"testing"
+)
+
+type lexTest struct {
+	name   string
+	input  string
+	tokens []token
+}
+
+var tEOF = token{tokenEOF, 0, ""}
+var tNL = token{tokenNL, 0, "\n"}
+var tCR = token{tokenCR, 0, "\r"}
+
+var lextTests = []lexTest{
+	{"empty", "", []token{tEOF}},
+	{"justText", "hello world", []token{{tokenText, 0, "hello world"}, tEOF}},
+	{"simpleLineCommentSlashNL", "//this is a comment\nHello World\n",
+		[]token{{tokenText, 0, "Hello World"}, tNL, tEOF}},
+	{"simpleLineCommentSlashCR", "//this is a comment\rHello World\r",
+		[]token{{tokenText, 0, "Hello World"}, tCR, tEOF}},
+	{"simpleLineCommentSlashCRNL", "//this is a comment\r\nHello World\r\n",
+		[]token{{tokenText, 0, "Hello World"}, tCR, tNL, tEOF}},
+	{"prePostLineCommentSlashNL", "//this is a comment\nHello World// another comment\n",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"prePostLineCommentSlashCR", "//this is a comment\rHello World// another comment\r",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"prePostLineCommentSlashCRNL", "//this is a comment\r\nHello World// another comment\r\n",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"simpleLineCommentHashNL", "#this is a comment\nHello World\n",
+		[]token{{tokenText, 0, "Hello World"}, tNL, tEOF}},
+	{"simpleLineCommentHashCR", "#this is a comment\rHello World\r",
+		[]token{{tokenText, 0, "Hello World"}, tCR, tEOF}},
+	{"simpleLineCommentHashCRNL", "#this is a comment\r\nHello World\r\n",
+		[]token{{tokenText, 0, "Hello World"}, tCR, tNL, tEOF}},
+	{"prePostLineCommentHashNL", "#this is a comment\nHello World# another comment\n",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"prePostLineCommentHashCR", "#this is a comment\rHello World# another comment\r",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"prePostLineCommentHashCRNL", "#this is a comment\r\nHello World# another comment\r\n",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"prePostLineCommentSlashHashNL", "//this is a comment\nHello World# another comment\n",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"prePostLineCommentSlashHashCR", "//this is a comment\rHello World# another comment\r",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"prePostLineCommentSlashHashCRNL", "//this is a comment\r\nHello World# another comment\r\n",
+		[]token{{tokenText, 0, "Hello World"}, tEOF}},
+	{"simpleBlockCommentNL", "/*this is a comment*/\nHello World\n",
+		[]token{tNL, {tokenText, 0, "Hello World"}, tNL, tEOF}},
+	{"simpleBlockCommentCR", "/*this is a comment*/\rHello World\r",
+		[]token{tCR, {tokenText, 0, "Hello World"}, tCR, tEOF}},
+	{"simpleBlockCommentCRNL", "/*this is a comment*/\r\nHello World\r\n",
+		[]token{tCR, tNL, {tokenText, 0, "Hello World"}, tCR, tNL, tEOF}},
+	{"prePostBlockCommentNL", "/*this is a comment\n*/Hello World/* another comment*/\n",
+		[]token{{tokenText, 0, "Hello World"}, tNL, tEOF}},
+	{"prePostBlockCommentCR", "/*this is a comment\r*/Hello World/* another comment*/\r",
+		[]token{{tokenText, 0, "Hello World"}, tCR, tEOF}},
+	{"prePostBlockCommentCRNL", "/*this is a comment\r\n*/Hello World/* another comment*/\r\n",
+		[]token{{tokenText, 0, "Hello World"}, tCR, tNL, tEOF}},
+	{"simpleBlockCommentMultiLineNL", "/*this\n is a\n comment\n*/Hello World\n",
+		[]token{{tokenText, 0, "Hello World"}, tNL, tEOF}},
+	{"simpleBlockCommentMultiLineCR", "/*this\r is a\r comment\r*/Hello World\r",
+		[]token{{tokenText, 0, "Hello World"}, tCR, tEOF}},
+	{"simpleBlockCommentMultiLineCRNL", "/*this\r\n is a\r\n comment\r\n*/Hello World\r\n",
+		[]token{{tokenText, 0, "Hello World"}, tCR, tNL, tEOF}},
+}
+
+// collect gathers the emitted items into a slice.
+func collect(t *lexTest, left, right string) (tokens []token) {
+	l := lex(t.name, t.input)
+	for {
+		token := l.nextToken()
+		tokens = append(tokens, token)
+		if token.typ == tokenEOF || token.typ == tokenError {
+			break
+		}
+	}
+	return
+}
+
+func equal(i1, i2 []token, checkPos bool) bool {
+	if len(i1) != len(i2) {
+		return false
+	}
+	for k := range i1 {
+		if i1[k].typ != i2[k].typ {
+			return false
+		}
+		if i1[k].value != i2[k].value {
+			return false
+		}
+		if checkPos && i1[k].pos != i2[k].pos {
+			return false
+		}
+	}
+	return true
+}
+
+func TestLex(t *testing.T) {
+	for _, test := range lextTests {
+		tokens := collect(&test, "", "")
+		if !equal(tokens, test.tokens, false) {
+			t.Errorf("%s: got \n\t%+v\nexpected\n\t%v", test.name, tokens, test.tokens)
+		}
+	}
+}
