5a79a2e00d02ca899b17a0932bc8b62be8e00d91
Joel Scoble <joel.scoble@outlook.com>
2017-02-17 13:03:57 -0600

    update to Apache-2.0 and add standard license header to the files; add a copy of Go's license as GO.LICENSE as the lexer is based on Go's text lexer

--- lex_test.go
+++ lex_test.go
@@ -1,12 +1,22 @@
-// Copyright 2015 Joel Scoble.
-// This code is governed by the MIT license, please
-// refer to the LICENSE file.
+// Copyright 2016 Joel Scoble
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License..
 //
 // the design of these tests is based on the tests in
 // http://golang.org/src/text/template/parse/lex_test.go
 // Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the GO.LICENSE file.
+
 package nocomment
 
 import (
